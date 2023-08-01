5eab7a46ae145bf92f1c9f4d7eb1616beaf38935
JAC0259613 <mamed14.pedro@gmail.com>
2023-08-01 09:59:15 -0300

    Foram feitos exercícios

---
+++ exercicio01.py
@@ -0,0 +1,12 @@
+# Wtapa 1: entrada de dados
+
+nota1 = float(input("Informe a sua primeira nota "))
+nota2 = float(input("Informe a sua segunda nota: "))
+nota3 = float(input("Informe a sua tercira nota: "))
+
+# Etapa 2: processamento de dados
+
+media = (nota1 + nota2 + nota3) / 3
+
+# Etapa 3:  Saída de dados (resposta na tela do usuário)
+print("Sua mádida é", media)
---
+++ exercico02.py
@@ -0,0 +1,5 @@
+numero = int(input("Informe o número um número "))
+
+antecessor = numero - 1
+sucessor = numero + 1
+print("seu número antecessor é ", antecessor, "e o sucessor ", sucessor)
